365e7a9694af643eddd6993d3e7319c12c161be3
patrache <ssk5442@naver.com>
2023-12-02 03:58:04 +0900

    [chore]: 나쁜말 삭제

--- frontend/src/app/dashboard/ec2/page.tsx
+++ frontend/src/app/dashboard/ec2/page.tsx
@@ -73,7 +73,7 @@ export default function DashBoardEc2() {
           case "console":
             return <>console</>;
           default:
-            return <>fuxk!</>;
+            return <>somethings wrong...</>;
         }
       })()}
       </InstanceDetail >
